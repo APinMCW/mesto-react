b9eab8c07926567a445a1448c2e4c0398da736b7
Aleksander Panferov <panferov.a.a@yandex.ru>
2023-01-18 23:56:19 +0300

    fix:name folder

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
 import PopupWithForm from "./PopupWithForm";
-import ImagePopup from "./ImagePopup";
+import ImagePopup from "./2134";
 
 function App() {
   const [isEditProfilePopupOpen, setEditProfilePopupOpen] = useState(false);
